145cc8e029a806a4f88c9bddf50de484f8e6e8f1
Joe <joe-sawyer32@users.noreply.github.com>
2017-06-16 13:42:30 -0500

    Button event listener set

--- main.js
+++ main.js
@@ -4,14 +4,26 @@
 
 // 1. First select and store the elements you'll be working with
 
-
 // 2. Create your `onSubmit` event for getting the user's search term
-
 
 // 3. Create your `fetch` request that is called after a submission
 
-
 // 4. Create a way to append the fetch results to your page
 
-
 // 5. Create a way to listen for a click that will play the song in the audio play
+
+var searchBox = document.querySelector("#userSearch");
+var submitBox = document.querySelector("#searchSubmit");
+var searchBoxValue;
+
+searchBox.addEventListener("click", function() {
+  searchBox.value = "";
+});
+
+submitBox.addEventListener("click", function(e) {
+  e.preventDefault();
+  searchBoxValue = searchBox.value;
+  console.log(searchBoxValue);
+});
+
+fetch();
