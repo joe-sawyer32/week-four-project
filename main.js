2e642ed4c85199202077c2108a8e1d8d21c05b42
Joe <joe-sawyer32@users.noreply.github.com>
2017-06-17 15:39:44 -0500

    List of usernames displayed

--- main.js
+++ main.js
@@ -14,17 +14,53 @@
 
 var searchBox = document.querySelector("#userSearch");
 var submitBox = document.querySelector("#searchSubmit");
+var results = document.querySelector(".results");
 var searchBoxValue;
 
 searchBox.addEventListener("click", function() {
   searchBox.value = "";
 });
 
-submitBox.addEventListener("click", function(e) {
-  e.preventDefault();
+submitBox.addEventListener("click", function() {
+  event.preventDefault();
   searchBoxValue = searchBox.value;
   performSearch(searchBoxValue);
 });
+
+function displayTracks(user) {
+  var url = "https://api.soundcloud.com/user";
+  fetch;
+}
+
+function displayArtists(artists) {
+  var artistOptionBlock = document.createElement("div");
+  artistOptionBlock.classList.add("artistOptions");
+
+  for (let i = 0; i < artists.length; i++) {
+    var thisArtist = artists[i];
+    var artist = thisArtist.username;
+    var artistTrackCount = thisArtist.track_count;
+    if (artistTrackCount > 0) {
+      var artistOption = document.createElement("div");
+      artistOption.classList.add("artistOption");
+      var user = document.createElement("h3");
+      user.classList.add("user");
+      user.innerHTML = "Username: " + artist;
+      var tracks = document.createElement("h4");
+      tracks.classList.add("tracks");
+      tracks.innerHTML = "Tracks: " + artistTrackCount;
+
+      artistOption.addEventListener("click", function() {
+        displayTracks(thisArtist.id);
+      });
+
+      artistOption.appendChild(user);
+      artistOption.appendChild(tracks);
+      artistOptionBlock.appendChild(artistOption);
+    }
+  }
+  results.appendChild(artistOptionBlock);
+}
 
 function performSearch(keyword) {
   var url =
@@ -36,7 +72,7 @@ function performSearch(keyword) {
       return response.json();
     })
     .then(function(data) {
-      console.log(data);
+      displayArtists(data);
     })
     .catch(function(error) {
       throw new Error("Your data request was unsuccessful...");
